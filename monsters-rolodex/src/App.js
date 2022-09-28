b36c8330b99f0758dc8151ac31478361aa8247e9
BigiCrab <jerry850408@gmail.com>
2022-09-29 00:48:39 +0800

    3-59 useEffect

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -4,16 +4,31 @@ import { Component } from "react";
 import CardList from "./components/card-list/card-list.component";
 import SearchBox from "./components/search-box/search-box.component";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const App = () => {
   const [searchField, setSearchField] = useState("");
-  console.log(searchField);
+  const [monsters, setMonsters] = useState([]);
+
+  console.log("render");
+
+  useEffect(() => {
+    console.log("fetch");
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => setMonsters(users));
+  }, []);
+
   const onSearchChange = (event) => {
     // console.log("search onChange:", event.target.value);
     const searchFieldString = event.target.value.toLowerCase();
     setSearchField(searchFieldString);
   };
+
+  const filteredMonsters = monsters.filter((monster) =>
+    monster.name.toLowerCase().includes(searchField)
+  );
+
   return (
     <div className="App">
       <h1 className="app-title">Monsters Rolodex</h1>
@@ -22,7 +37,7 @@ const App = () => {
         placeholder="search monster"
         className="monster-search-box"
       ></SearchBox>
-      {/* <CardList monsters={filteredMonsters}></CardList> */}
+      <CardList monsters={filteredMonsters}></CardList>
     </div>
   );
 };
