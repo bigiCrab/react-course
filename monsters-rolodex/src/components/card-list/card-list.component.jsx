c7e6bf4ded5b2f0fcbb62c6130792d5dded770e3
BigiCrab <jerry850408@gmail.com>
2022-09-29 01:13:30 +0800

    3-61 change to functional component

--- monsters-rolodex/src/components/card-list/card-list.component.jsx
+++ monsters-rolodex/src/components/card-list/card-list.component.jsx
@@ -1,18 +1,12 @@
-import { Component } from "react";
 import "./card-list.styles.css";
 import Card from "../card/card.component";
-class CardList extends Component {
-  render() {
-    // console.log("render");
-    const { monsters } = this.props;
-    return (
-      <div className="card-list">
-        {monsters.map((monster) => {
-          return <Card monster={monster} key={monster.id}></Card>;
-        })}
-      </div>
-    );
-  }
-}
+
+const CardList = ({ monsters }, forwardRef) => (
+  <div className="card-list">
+    {monsters.map((monster) => {
+      return <Card monster={monster} key={monster.id}></Card>;
+    })}
+  </div>
+);
 
 export default CardList;
