142e192726c8fa1a8339ce68441bf4cf5e3052ae
BigiCrab <jerry850408@gmail.com>
2022-09-28 17:56:28 +0800

    fix

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -6,25 +6,31 @@ class App extends Component {
   constructor() {
     super();
 
-    this.state = [
-      {
-        name: "monster1",
-        id: "id_m1",
-      },
-      {
-        name: "monster2",
-        id: "id_m2",
-      },
-      {
-        name: "monster3",
-        id: "id_m3",
-      },
-    ];
+    this.state = {
+      monsters: [
+        {
+          name: "monster1",
+          id: "id_m1",
+        },
+        {
+          name: "monster2",
+          id: "id_m2",
+        },
+        {
+          name: "monster3",
+          id: "id_m3",
+        },
+        {
+          name: "monster4",
+          id: "id_m4",
+        },
+      ],
+    };
   }
   render() {
     return (
       <div className="App">
-        {this.state.map((monster) => {
+        {this.state.monsters.map((monster) => {
           return (
             <div key={monster.id}>
               <h1>{monster.name}</h1>
